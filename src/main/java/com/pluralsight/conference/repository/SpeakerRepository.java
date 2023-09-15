4a9acc2ad7b4e1f859ae37af3381854b52d9a7cf
MrPancetita <contreras.engineer@outlook.com>
2023-09-15 21:36:46 +0200

    in dev progress

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepository.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepository.java
@@ -3,7 +3,10 @@ package com.pluralsight.conference.repository;
 import com.pluralsight.conference.model.Speaker;
 
 import java.util.List;
+/*Repository class to interact with DB*/
 
 public interface SpeakerRepository {
     List<Speaker> findAll();
+
+    Speaker create(Speaker speaker);
 }
--- src/main/java/com/pluralsight/conference/service/SpeakerService.java
+++ src/main/java/com/pluralsight/conference/service/SpeakerService.java
@@ -3,7 +3,10 @@ package com.pluralsight.conference.service;
 import com.pluralsight.conference.model.Speaker;
 
 import java.util.List;
+/*Service class for business logic */
 
 public interface SpeakerService {
     List<Speaker> findAll();
+
+    Speaker create(Speaker speaker);
 }
